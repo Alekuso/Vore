5e0e15ae48f2908639dba929e650b6a67c618602
Alexou <48645888+Alekuso@users.noreply.github.com>
2020-08-28 23:50:19 +0200

    uploading content

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("*Vore is loading...*")
+const {Client} = require("discord.js");
+const bot = new Client()
+
+bot.on('ready', () => {
+    console.log("VORE ME DADDY")
+})
+
+bot.on('message', message => {
+    if(message.author.bot) return;
+
+    if(message.content.startsWith("!vore")) {
+        let member = message.mentions.members.first();
+        if(!member) return message.channel.send(`*vores ${message.author.username}*`)
+
+        message.channel.send(`*vores ${member.user.username}*`)
+    }else if(message.content.toUpperCase().includes("VORE")) {
+        message.channel.send(`*vores ${message.author.username}*`)
+    }
+
+
+})
+
+bot.login(require('./config.json').token)
